5b448debb7b26f479e1e3b6d8b9edef198ceb0d8
dhrebeniuk <dmytrohrebeniuk@gmail.com>
2022-10-25 11:36:40 +0300

    No need define copy contractor
    Resolved: definition of implicit copy assignment operator for 'Proxy' is deprecated because it has a user-provided copy constructor.
    
    https://en.cppreference.com/w/cpp/language/copy_constructor

--- cpp/flood/include/IPPort.h
+++ cpp/flood/include/IPPort.h
@@ -23,11 +23,6 @@ public:
 		:address(address)
 		,port(port)
 	{};
-
-	IPPort(const IPPort& other)
-		:address(other.address)
-		,port(other.port)
-	{};
 	
 	const string& getAddress() const { return this->address; };
 	const uint16_t& getPort() const { return this->port; };
