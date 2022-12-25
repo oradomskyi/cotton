c92412e76ec750ef35e323e47a4c5249e6d23360
Oleksandr Radomskyi <o.radomskyi@gmail.com>
2022-12-25 23:32:26 +0100

    updated unpack_resources.sh

--- cpp/unpack_resources.sh
+++ cpp/unpack_resources.sh
@@ -1,4 +1,4 @@
 mkdir 3rdParty
-
+mkdir 3rdParty/boost
 unzip ../resources/3rdParty/boost_1.18.0_all_cpp11_include -d 3rdParty/boost/
 unzip ../resources/3rdParty/boost_1.18.0_all_cpp11_lib_static -d 3rdParty/boost/
