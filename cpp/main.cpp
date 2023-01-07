3033ceaae3b2fc19fbc257b8456201d36a15f989
Oleksandr Radomskyi <oleksandr.radomskyi@embl.it>
2023-01-07 10:45:54 -0500

    removed commented code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,20 +2,6 @@
 
 int main()
 {
-	//test_init_worker();
-	//test_memory_create_destroy();
-
-	//test_check_Target_security();
-	//test_check_Proxy_security();
-
-    //test_threadmaster_init_with_floods();
-    //test_threadmaster_run();
-    //test_threadmaster_long_run_create_destroy();
-    //test_threadmaster_long_run();
-    //test_parallelposix_startstop();
-	
-	//test_parallelboostfiber_include_boost();
-    
     test_parallelboostfiber_run();
 
 	return 0;
